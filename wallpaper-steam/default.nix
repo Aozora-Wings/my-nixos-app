7e3fff617ab8af369be69f2e0310c1dcbea86807
wt <wt@qkzy.net>
2025-07-22 17:55:55 +0800

    test

--- wallpaper-steam/default.nix
+++ wallpaper-steam/default.nix
@@ -1,20 +1,28 @@
-{
-  extra-cmake-modules,
-  fetchFromGitHub,
-  kpackage,
-  libplasma,
-  lib,
-  lz4,
-  mkKdeDerivation,
-  mpv-unwrapped,
-  pkg-config,
-  python3,
-  qtbase,
-  qtmultimedia,
-  qtwebchannel,
-  qtwebengine,
-  qtwebsockets,
+{ lib
+, fetchFromGitHub
+, kpackage
+, libplasma
+, lz4
+, mkKdeDerivation
+, mpv-unwrapped
+, pkg-config
+, python3
+, qtbase
+, qtmultimedia
+, qtwebchannel
+, qtwebengine
+, qtwebsockets
+, wrapPython
+, makeWrapper
 }:
+
+let
+  pythonEnv = python3.withPackages (ps: with ps; [
+    websockets
+    pyqt5  # 如果需要 Qt Python 绑定
+  ]);
+in
+
 mkKdeDerivation {
   pname = "wallpaper-engine-kde-plugin-my";
   version = "0.5.5-unstable-2024-11-03";
@@ -29,17 +37,20 @@ mkKdeDerivation {
 
   patches = [ ./nix-plugin.patch ];
 
-  extraNativeBuildInputs = [
+  nativeBuildInputs = [
     kpackage
     pkg-config
-    (python3.withPackages (ps: with ps; [ websockets ]))
+    python3
+    wrapPython
+    makeWrapper
   ];
 
-  extraBuildInputs = [
+  buildInputs = [
     extra-cmake-modules
     libplasma
     lz4
     mpv-unwrapped
+    pythonEnv  # 将 Python 环境作为构建输入
   ];
 
   extraCmakeFlags = [
@@ -54,17 +65,26 @@ mkKdeDerivation {
     (lib.cmakeFeature "Qt6_DIR" "${qtbase}/lib/cmake/Qt6")
   ];
 
-postInstall = ''
-  cd $out/share/plasma/wallpapers/com.github.catsout.wallpaperEngineKde
-  chmod +x ./contents/pyext.py
-  
-  wrapProgram ./contents/pyext.py \
-    --prefix PYTHONPATH : "${python3.withPackages (ps: [ ps.websockets ])}/${python3.sitePackages}"
-  
-  substituteInPlace ./contents/ui/Pyext.qml \
-    --replace-fail NIX_STORE_PACKAGE_PATH ${placeholder "out"}
-  cd -
-'';
+  postInstall = ''
+    cd $out/share/plasma/wallpapers/com.github.catsout.wallpaperEngineKde
+    
+    # 设置 Python 脚本权限
+    chmod +x ./contents/pyext.py
+    
+    # 修正 shebang
+    patchShebangs ./contents/pyext.py
+    
+    # 包装 Python 脚本，确保运行时能找到依赖
+    wrapProgram ./contents/pyext.py \
+      --prefix PYTHONPATH : "${pythonEnv}/${python3.sitePackages}" \
+      --prefix PATH : "${lib.makeBinPath [ python3 ]}"
+    
+    # 替换 Nix 存储路径
+    substituteInPlace ./contents/ui/Pyext.qml \
+       --replace-fail NIX_STORE_PACKAGE_PATH "$out"
+    
+    cd -
+  '';
 
   meta = with lib; {
     description = "KDE wallpaper plugin integrating Wallpaper Engine";
