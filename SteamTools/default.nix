6d1d9a61af6767e31b55aca66c154e857997082c
wt <wt@qkzy.net>
2026-09-11 13:25:02 +0800

    更新 Steam++ SRI（服务式分支完整版，含 systemd 服务触发）

--- SteamTools/default.nix
+++ SteamTools/default.nix
@@ -9,7 +9,7 @@ let
 
   src = pkgs.fetchurl {
     url = "https://oazcc.qzapp.qkzy.net/Steam++.tgz";
-    sha256 = "sha256-bcgjuGQ4sYClQQTBgUNTwpkfgJNkh6fJGxWxF9nx338=";
+    sha256 = "sha256-vX765OTgrO9/JgFYhkti6tnDzRt72MsULfVAAlSzX/k=";
   };
 
   unpacked = pkgs.runCommand "steam++-unpacked" {} ''
