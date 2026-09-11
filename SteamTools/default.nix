ddadb84e20945ea88f18a42e500f40919f661f84
wt <wt@qkzy.net>
2026-09-11 20:55:34 +0800

    SteamTools: SRI update (hosts dump NixOS branch, 2051484223961)

--- SteamTools/default.nix
+++ SteamTools/default.nix
@@ -9,7 +9,7 @@ let
 
   src = pkgs.fetchurl {
     url = "https://oazcc.qzapp.qkzy.net/Steam++.tgz";
-    sha256 = "sha256-5ItqLcyhsZ7aAHEu7CReQVBWnU6XadMv4golr63INOM=";
+    sha256 = "sha256-xFRe6JGNHm+mxoeUiSdaLPW9Obl6a1v0fQYxsgJB7fc=";
   };
 
   unpacked = pkgs.runCommand "steam++-unpacked" {} ''
