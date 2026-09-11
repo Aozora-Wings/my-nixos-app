c213a219d758687fa044990ada819a9bb0eb5662
wt <wt@qkzy.net>
2026-09-11 08:28:44 +0800

    SteamTools: 修复加速器子进程启动（NixOS 跳过提权序列化，保留 DOTNET_ROOT）+ hosts NixOS 分支（写入配置片段供 networking.hosts 声明式管理）

--- SteamTools/default.nix
+++ SteamTools/default.nix
@@ -9,7 +9,7 @@ let
 
   src = pkgs.fetchurl {
     url = "https://oazcc.qzapp.qkzy.net/Steam++.tgz";
-    sha256 = "sha256-5l2nESCaBKk5y/duY461K+8zSPcNSdZJeMLBxjHy+S8=";
+    sha256 = "sha256-63h+8K27jE+jQiNjcidkoLhCAg/rRooEhji5Pl3jh1g=";
   };
 
   unpacked = pkgs.runCommand "steam++-unpacked" {} ''
